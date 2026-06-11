161258f6b52ab87829ed3c24473b0a5962b098a8
santosh696242-code <santosh696242@gmail.com>
2026-06-11 13:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -611,6 +611,7 @@ def master_help(message):
         "<b>4. Group ya Private me AI Kaise use karein:</b>\n"
         "• <b>Private:</b> Customers direct bot ko /start bhej kar baat kar sakte hain.\n"
         "• <b>Groups:</b> Apne bot ko group me add karein aur use Admin permissions dein.\n\n"
+        "🤖 <b>@santosh_devloperbot Use this for More information.</b>\n\n"
         f"📞 Support support ke liye <code>/contact</code> command ka use karein."
     )
     master_bot.reply_to(message, help_text, parse_mode="HTML")
